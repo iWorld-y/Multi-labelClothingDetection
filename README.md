e16e2ed11b6188b43327447195891b0ff8ab18ad
iWorld Jiang <caima12138@foxmail.com>
2023-04-15 21:04:12 +0800

    可视化代码草稿

--- README.md
+++
@@ -1,2 +0,0 @@
-# 多标签服饰检测
-./Format.py 以及 ./example.py 为将 DeepFasion2 数据集转为 COCO 格式的操作. 
---
+++ code/可视化.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+"""
+@Time    : 2023/4/15 20:56
+@Author  : Eugene
+@FileName: 可视化.py 
+"""
+# %%
+data = pd.read_csv(r"runs/detect/train4/results.csv")
