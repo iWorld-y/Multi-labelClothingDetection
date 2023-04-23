56234231149c2720a3eceb6f717e3ac75c13812e
iWorld-y <caima12138@foxmail.com>
2023-04-23 16:33:30 +0800

    开始第一次 YOLOv5 训练; 修改了训练结束扫尾脚本.

--- code/yolov5/v5_train.sh
+++ code/yolov5/v5_train.sh
@@ -1,1 +1,27 @@
-python ~/code/yolov5-master/train.py --batch-size 192 --data dataset/fashion2 --img 640 --epochs 100 --weight models/yolov5n.pt
+#!/bin/bash
+
+# 获取第一个参数，作为 Git 提交的消息
+message="$1"
+
+# 检查是否提供了 Git 提交的消息
+if [ -z "$message" ]; then
+  echo "请提供 Git 提交的消息作为第一个参数"
+  exit 1
+fi
+
+# 运行 YOLOv5 训练脚本
+python ~/code/yolov5/train.py \
+    --batch-size 192 \
+    --data dataset/fashion2.yaml \
+    --img 640 \
+    --epochs 100 \
+    --weight models/yolov5n.pt \
+    --project runs/v5 ;
+
+# 提交 Git 更改
+git add ~/code/Multi-labelClothingDetection ;
+git commit -m "$message" ;
+git push origin main ;
+
+# 关闭计算机
+/usr/bin/shutdown
