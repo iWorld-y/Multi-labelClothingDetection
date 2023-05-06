59040ab7c7daad9a55cc91a9df04121dcd8ba2f8
iWorld-y <caima12138@foxmail.com>
2023-05-07 02:36:59 +0800

    修改 DeepFashion2 转 YOLO 代码

--- code/convert_dataset/deepfashion2yolo.py
+++ code/convert_dataset/deepfashion2yolo.py
@@ -23,34 +23,35 @@ def listPathAllfiles(dirname):
 
 
 if __name__ == '__main__':
-    annos_path = rf"{data_path}/annos"  # 改成需要路径
-    image_path = rf"{data_path}/image"  # 改成需要路径
-    labels_path = rf"{data_path}/labels"  # 改成需要路径
+    for data_path in [train_data_path, vaild_data_path]:
+        annos_path = rf"{data_path}/annos"  # 改成需要路径
+        image_path = rf"{data_path}/image"  # 改成需要路径
+        labels_path = rf"{data_path}/labels"  # 改成需要路径
 
-    num_images = len(os.listdir(annos_path))
+        num_images = len(os.listdir(annos_path))
 
-    for num in tqdm(range(1, num_images + 1)):
-        json_name = os.path.join(annos_path, str(num).zfill(6) + '.json')
-        image_name = os.path.join(image_path, str(num).zfill(6) + '.jpg')
-        txtfile = os.path.join(labels_path, str(num).zfill(6) + '.txt')
-        imag = Image.open(image_name)
-        width, height = imag.size
+        for num in tqdm(range(1, num_images + 1)):
+            json_name = os.path.join(annos_path, str(num).zfill(6) + '.json')
+            image_name = os.path.join(image_path, str(num).zfill(6) + '.jpg')
+            txtfile = os.path.join(labels_path, str(num).zfill(6) + '.txt')
+            imag = Image.open(image_name)
+            width, height = imag.size
 
-        res = []
-        with open(json_name, 'r') as f:
-            temp = json.loads(f.read())
-            for i in temp:
-                if i == 'source' or i == 'pair_id':
-                    continue
-                else:
-                    box = temp[i]['bounding_box']
-                    x_1 = round((box[0] + box[2]) / 2 / width, 6)
-                    y_1 = round((box[1] + box[3]) / 2 / height, 6)
-                    w = round((box[2] - box[0]) / width, 6)
-                    h = round((box[3] - box[1]) / height, 6)
+            res = []
+            with open(json_name, 'r') as f:
+                temp = json.loads(f.read())
+                for i in temp:
+                    if i == 'source' or i == 'pair_id':
+                        continue
+                    else:
+                        box = temp[i]['bounding_box']
+                        x_1 = round((box[0] + box[2]) / 2 / width, 6)
+                        y_1 = round((box[1] + box[3]) / 2 / height, 6)
+                        w = round((box[2] - box[0]) / width, 6)
+                        h = round((box[3] - box[1]) / height, 6)
 
-                    category_id = int(temp[i]['category_id'] - 1)
+                        category_id = int(temp[i]['category_id'] - 1)
 
-                    res.append(" ".join([str(category_id), str(x_1), str(y_1), str(w), str(h)]))
+                        res.append(" ".join([str(category_id), str(x_1), str(y_1), str(w), str(h)]))
 
-        open(txtfile, "w").write("\n".join(res))
+            open(txtfile, "w").write("\n".join(res))
